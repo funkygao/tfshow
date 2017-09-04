772b15cbc07575b205cf5426d435d91e2cc4b8f8
Funky Gao <funky.gao@gmail.com>
2017-09-04 13:41:08 +0800

    nil

--- raw/linear.py
+++ raw/linear.py
@@ -5,13 +5,16 @@ from perceptron import Perceptron
 import matplotlib.pyplot as plt
 import numpy as np
 
-f = lambda x: x
+f = lambda x: x # 激活函数
 
 class LinearUnit(Perceptron):
 	def __init__(self, input_num):
 		Perceptron.__init__(self, input_num, f)
 
 def generate_samples():
+	'''
+	生成5个人的样本数据：工龄，与月工资
+	'''
 	return [[5], [3], [8], [1.4], [10.1]], [5500, 2300, 7600, 1800, 11400]
 
 if __name__ == '__main__':
@@ -24,17 +27,12 @@ if __name__ == '__main__':
 	print 'Work 1.5 years, monthly salary = %.2f' % l.predict([1.5])
 	print 'Work 6.3 years, monthly salary = %.2f' % l.predict([6.3])
 
+	# plot
 	x = np.arange(0, 13)
-	y = []
-	for i in range(len(x)):
-		y.append(l.predict([x[i]]))
+	y = [l.predict([x_]) for x_ in x]	
 	plt.plot(x, y, label='linear')
-	x, y = [], []
-	for m in train_x:
-		x.append(m[0])
-	for m in train_y:
-		y.append(m)
-	plt.plot(x, y, 'ro', label='sample %d'%len(x))
+	x = map(lambda x_:x_[0], train_x)	
+	plt.plot(x, train_y, 'ro', label='sample %d'%len(x))
 	plt.legend()
 	plt.show()
     
