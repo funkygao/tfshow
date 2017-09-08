25c97fdca494c545c4459f49a5da249ac5cad08a
Funky Gao <funky.gao@gmail.com>
2017-09-08 16:26:56 +0800

    interactive matplot render

--- raw/draw_mnist.py
+++ raw/draw_mnist.py
@@ -3,20 +3,39 @@
 
 import mnist_loader
 import numpy as np
+import matplotlib.pyplot as plt
 import time
 import os
 
+# Configurations
+enable_matplot = True
+
+if enable_matplot:
+    plt.ion()
+    figure = plt.figure()
+    ax1 = plt.subplot(211)
+    ax1.set_title('mnist image')
+
 train_data, _, _ = mnist_loader.load_data_wrapper()
-for n in range(len(train_data)):
+dim = int(np.sqrt(len(train_data[0][0])))
+
+for n in range(len(train_data)):    
+    img = train_data[n][0].reshape(dim, dim)
+
+    time.sleep(.1)
+
+    # draw on matplot
+    if enable_matplot:
+        ax1.matshow(img, cmap=plt.get_cmap('gray'))
+        figure.canvas.draw()
+        continue
+
     os.system('clear')
-    sample = train_data[n][0].reshape(28, 28)
-    for y in range(28):
-        for x in range(28):
-            if sample[y][x] > .1:
+    for y in range(dim):
+        for x in range(dim):
+            if img[y][x] > .1:
                 print 'X',
             else:
                 print '.',
         print
-    #print np.argmax(train_data[n][1])
-    print n+1
-    time.sleep(0.1)
+    print n+1, np.argmax(train_data[n][1])
